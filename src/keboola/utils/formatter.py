b02e88a1484c1b98471db9737b1b7539c4be3ba6
Adam Bako <bakoad@gmail.com>
2021-03-25 14:47:05 +0100

    added unit test

--- src/keboola/utils/formatter.py
+++ src/keboola/utils/formatter.py
@@ -2,6 +2,8 @@ from typing import List
 
 
 def comma_separated_values_to_list(csv_string: str) -> List:
+    if not isinstance(csv_string, str):
+        raise TypeError("input must be a string")
     cols = []
     if csv_string:
         cols = [p.strip() for p in csv_string.split(",")]
---
+++ tests/test_formatter.py
@@ -0,0 +1,17 @@
+import unittest
+import keboola.utils.formatter as futils
+
+
+class TestFormatterUtils(unittest.TestCase):
+    def test_comma_separated_values_to_list(self):
+        csv_string_1 = '1,2,3,4,5'
+        csv_list_1 = futils.comma_separated_values_to_list(csv_string_1)
+        self.assertEqual(csv_list_1, ['1', '2', '3', '4', '5'])
+        csv_string_2 = 'name,age,email'
+        csv_list_2 = futils.comma_separated_values_to_list(csv_string_2)
+        self.assertEqual(csv_list_2, ['name', 'age', 'email'])
+        csv_string_3 = 'asdh7,asdhj$#@,p_.sd,96,5'
+        csv_list_3 = futils.comma_separated_values_to_list(csv_string_3)
+        self.assertEqual(csv_list_3, ['asdh7', 'asdhj$#@', 'p_.sd', '96', '5'])
+        with self.assertRaises(TypeError):
+            futils.comma_separated_values_to_list(123)
