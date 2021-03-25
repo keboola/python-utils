6392ce496c9c152674f6ed6daabab9c252fae536
Adam Bako <bakoad@gmail.com>
2021-03-25 15:36:14 +0100

    split test

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -13,5 +13,7 @@ class TestFormatterUtils(unittest.TestCase):
         csv_string_3 = 'asdh7,asdhj$#@,p_.sd,96,5'
         csv_list_3 = futils.comma_separated_values_to_list(csv_string_3)
         self.assertEqual(csv_list_3, ['asdh7', 'asdhj$#@', 'p_.sd', '96', '5'])
+
+    def test_comma_separated_values_to_list_throws_type_error(self):
         with self.assertRaises(TypeError):
             futils.comma_separated_values_to_list(123)
